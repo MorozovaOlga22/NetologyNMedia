1a2b6514277a2a9c800f153528b2424667109625
Olga Morozova <Morozova_Olga_22@mail.ru>
2021-10-20 21:39:55 +0300

    SharedPreferences added

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,13 +1,14 @@
 package ru.netology.nmedia
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositoryInSharedPrefs
 
-class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryInSharedPrefs(application)
     val data: LiveData<List<Post>> = repository.get()
 
     fun likeById(id: Long) = repository.likeById(id)
---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInSharedPrefs.kt
@@ -0,0 +1,111 @@
+package ru.netology.nmedia.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.dto.Post
+import java.lang.RuntimeException
+
+class PostRepositoryInSharedPrefs(context: Context) : PostRepository {
+    private val prefs = context.getSharedPreferences("repo", Context.MODE_PRIVATE)
+    private val key = "posts"
+
+    private val gson = Gson()
+    private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+
+    private val emptyPost = Post(
+        id = 0,
+        author = "",
+        content = "",
+        published = "",
+    )
+
+    private var nextId = 1L
+
+    private val data = MutableLiveData(emptyList<Post>())
+
+    init {
+        prefs.getString(key, null)?.let { postsJson ->
+            try {
+                val posts: List<Post> = gson.fromJson(postsJson, type)
+                data.value = posts
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun get(): LiveData<List<Post>> = data
+
+    override fun likeById(id: Long) {
+        val updater = { post: Post ->
+            if (post.id == id)
+                post.copy(
+                    likesCount = if (post.likedByMe) post.likesCount - 1 else post.likesCount + 1,
+                    likedByMe = !post.likedByMe
+                )
+            else
+                post
+        }
+        updatePost(updater)
+    }
+
+    override fun repostById(id: Long) {
+        val updater = { post: Post ->
+            if (post.id == id)
+                post.copy(repostsCount = post.repostsCount + 1)
+            else
+                post
+        }
+        updatePost(updater)
+    }
+
+    override fun removeById(id: Long) {
+        val posts = getPostsFromLiveData()
+        val updatedPosts = posts.filter { post -> post.id != id }
+        data.value = updatedPosts
+        sync()
+    }
+
+    override fun save(postId: Long, newContent: String) {
+        if (postId == 0L) {
+            val newPost = emptyPost.copy(
+                id = nextId++,
+                author = "Me",
+                published = "now",
+                content = newContent
+            )
+            val posts = listOf(newPost) + getPostsFromLiveData()
+            data.value = posts
+            sync()
+            return
+        }
+
+        val updater = { oldPost: Post ->
+            if (oldPost.id == postId)
+                oldPost.copy(content = newContent)
+            else
+                oldPost
+        }
+        updatePost(updater)
+    }
+
+    private fun sync() {
+        prefs.edit().apply {
+            val postsJson = gson.toJson(data.value)
+            putString(key, postsJson)
+            apply()
+        }
+    }
+
+    private fun updatePost(updater: (Post) -> Post) {
+        val posts = getPostsFromLiveData()
+        val updatedPosts = posts.map(updater)
+        data.value = updatedPosts
+        sync()
+    }
+
+    private fun getPostsFromLiveData() = data.value ?: throw RuntimeException("Data not specified")
+}
